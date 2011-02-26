a91b9de4fdfa7a094f3ff2747dacfe2222736efe
Richard S <voidet@nightgen.com>
2011-02-26 18:16:21 +1000

    Initial Commit

---
+++ controllers/components/form_keeper.php
@@ -0,0 +1,117 @@
+<?php
+
+class FormKeeperComponent extends Dispatcher {
+
+	public function startup(&$controller) {
+		$default = array(
+			'salt' => Configure::read('Security.salt'),
+			'cacheKey' => 'default',
+		);
+
+		if (Configure::load('form_keeper')) {
+			$settings = Configure::read('FormKeeper');
+		} else {
+			$settings = array();
+		}
+
+		$this->settings = array_merge($settings, $default);
+
+		$this->stitchFields($_POST);
+		debug(parent::parseParams($this->getUrl()));
+	}
+
+	public function stitchFields(&$data) {
+		extract($this->settings);
+
+		$cachedName = Cache::read(Security::hash('fieldMaps'.$salt, null, false), $cacheKey);
+		if (!empty($data)) {
+			foreach ($data as $hash => $field) {
+				if (in_array($hash, $cachedName)) {
+
+				}
+			}
+		}
+
+	}
+
+	public function parseParams(&$controller) {
+
+		debug($controller->_POST);
+
+		if (empty($controller->data)) {
+			return true;
+		}
+		$data = $controller->data;
+
+		if (!isset($data['_Token']) || !isset($data['_Token']['fields']) || !isset($data['_Token']['key'])) {
+			return false;
+		}
+		$token = $data['_Token']['key'];
+
+		if ($this->Session->check('_Token')) {
+			$tokenData = unserialize($this->Session->read('_Token'));
+
+			if ($tokenData['expires'] < time() || $tokenData['key'] !== $token) {
+				return false;
+			}
+		}
+
+		$locked = null;
+		$check = $controller->data;
+		$token = urldecode($check['_Token']['fields']);
+
+		if (strpos($token, ':')) {
+			list($token, $locked) = explode(':', $token, 2);
+		}
+		unset($check['_Token']);
+
+		$locked = explode('|', $locked);
+
+		$lockedFields = array();
+		$fields = Set::flatten($check);
+		$fieldList = array_keys($fields);
+		$multi = array();
+
+		foreach ($fieldList as $i => $key) {
+			if (preg_match('/\.\d+$/', $key)) {
+				$multi[$i] = preg_replace('/\.\d+$/', '', $key);
+				unset($fieldList[$i]);
+			}
+		}
+		if (!empty($multi)) {
+			$fieldList += array_unique($multi);
+		}
+
+		foreach ($fieldList as $i => $key) {
+			$isDisabled = false;
+			$isLocked = (is_array($locked) && in_array($key, $locked));
+
+			if (!empty($this->disabledFields)) {
+				foreach ((array)$this->disabledFields as $disabled) {
+					$disabled = explode('.', $disabled);
+					$field = array_values(array_intersect(explode('.', $key), $disabled));
+					$isDisabled = ($field === $disabled);
+					if ($isDisabled) {
+						break;
+					}
+				}
+			}
+
+			if ($isDisabled || $isLocked) {
+				unset($fieldList[$i]);
+				if ($isLocked) {
+					$lockedFields[$key] = $fields[$key];
+				}
+			}
+		}
+		sort($fieldList, SORT_STRING);
+		ksort($lockedFields, SORT_STRING);
+
+		$fieldList += $lockedFields;
+		$check = Security::hash(serialize($fieldList) . Configure::read('Security.salt'));
+		return ($token === $check);
+	}
+
+
+
+}
---
+++ views/helpers/form_keeper.php
@@ -0,0 +1,75 @@
+<?php
+
+class FormKeeperHelper extends FormHelper {
+
+	public $settings = array();
+
+	public function beforeRender() {
+
+		$default = array(
+			'salt' => Configure::read('Security.salt'),
+			'cacheKey' => 'default',
+		);
+
+		if (Configure::load('form_keeper')) {
+			$settings = Configure::read('FormKeeper');
+		} else {
+			$settings = array();
+		}
+
+		$this->settings = array_merge($settings, $default);
+
+	}
+
+	public function _name($options = array(), $field = null, $key = 'name') {
+		extract($this->settings);
+		$view =& ClassRegistry::getObject('view');
+		if ($options === null) {
+			$options = array();
+		} elseif (is_string($options)) {
+			$field = $options;
+			$options = 0;
+		}
+
+		if (!empty($field)) {
+			$this->setEntity($field);
+		}
+
+		if (is_array($options) && array_key_exists($key, $options)) {
+			return $options;
+		}
+
+		switch ($field) {
+			case '_method':
+				$name = $field;
+			break;
+			default:
+				$name = 'data[' . implode('][', $view->entity()) . ']';
+			break;
+		}
+
+		$cachedName = Cache::read(Security::hash('fieldMaps'.$salt, null, false), $cacheKey);
+		if (!empty($cachedName)) {
+			$cachedName = array_flip($cachedName);
+		}
+		if (empty($cachedName[$name])) {
+			$hashKey = Security::hash($name.$salt, null, false);
+			$cachedName[$hashKey] = $name;
+			$cachedName = array_flip($cachedName);
+			Cache::write(Security::hash('fieldMaps'.$salt, null, false), $cachedName, $cacheKey);
+		} else {
+			$hashKey = $cachedName[$name];
+		}
+
+		$name = $hashKey;
+		$options['id'] = $name;
+
+		if (is_array($options)) {
+			$options[$key] = $name;
+			return $options;
+		} else {
+			return $name;
+		}
+	}
+
+}
